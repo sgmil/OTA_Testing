06fcd94bff4928f3a3fefd12622a755d0bfe7435
sgmil <smilheim@gmail.com>
2020-01-03 21:17:41 -0700

    changed to #define

--- MyBlinkOTA/MyBlinkOTA.ino
+++ MyBlinkOTA/MyBlinkOTA.ino
@@ -1,15 +1,19 @@
-// Test OTA using My_ESP8266_Functions
 ADC_MODE(ADC_VCC)
+#define BOARD esp8266:esp8266:nodemcuv2
+#define IP_LAST_THREE 177
+#define PORT /dev/ttyUSB0
+
+// Test OTA using My_ESP8266_Functions
+
 
 #include "My_ESP8266_Functions.h"
-#include <string>
 
-int IPlastThree=177;
+int IPlastThree=IP_LAST_THREE;
 MyWiFi wifi("ESP8266",IPlastThree);
 
 const byte led = 16;
 unsigned long previousTime = millis();
-const unsigned long interval = 1000;
+const unsigned long interval = 250;
 
 void setup() {
   Serial.begin(115200); 
